866ed2ea20477e18caca84938a3f195c149b05d0
kumar9034 <kumarrajput040202003@gmail.com>
2025-06-29 18:55:47 +0530

    refactor: update createRoot import to use ReactDOM in main.jsx

--- myweb/src/main.jsx
+++ myweb/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import Index from './index.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
     <BrowserRouter>
       <Index />
